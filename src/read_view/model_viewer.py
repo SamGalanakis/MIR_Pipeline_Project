91324df35f1df646067410eb0d128c2962c0817d
Samuel Galanakis <sammeg.alanakis6@gmail.com>
2020-09-08 17:12:21 +0200

    modes

--- src/read_view/model_viewer.py
+++ src/read_view/model_viewer.py
@@ -5,8 +5,26 @@ from OpenGL.GL.shaders import compileProgram, compileShader
 from glfw import PRESS
 import numpy as np
 import pyrr
+from pathlib import Path
 from read_file import read_file
 from input_handler import input_handler
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('f', type=str, help='file path')
+
+args = parser.parse_args()
+
+path =  Path(args.f)
+
+
+# path = str(Path("data/test_ply.ply"))
+
+
+
+
 
 vertex_src = """
 # version 330
@@ -39,8 +57,8 @@ def window_resize(window, width, height):
     glViewport(0, 0, width, height)
     projection = pyrr.matrix44.create_perspective_projection_matrix(45, width / height, 0.1, 100)
    # projection = pyrr.matrix44.create_orthogonal_projection_matrix(0,1280,0,720,-1000,1000)
-    window_height = glfw.get_window_size(window)[1]
-    window_width = glfw.get_window_size(window)[0]
+    window_height = height
+    window_width = width
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
 
 # initializing glfw library
@@ -66,8 +84,7 @@ glfw.set_window_size_callback(window, window_resize)
 
 glfw.make_context_current(window)
 
-path = r"data\benchmark\db\0\m0\m0.off"
-path =r"data\test_ply.ply"
+
 
 vertices, indices, info = read_file(path)
 
@@ -77,6 +94,8 @@ mean_point = as_points.mean(axis=0)
 
 max_x, max_y,max_z = as_points.max(axis=0)
 min_x, min_y , min_z = as_points.min(axis=0)
+
+middle_point = np.array([min_x + (max_x-min_x)/2 , min_y + (max_y-min_y)/2 , min_z + (max_z-min_z)/2] )
 
 
 
@@ -124,115 +143,83 @@ window_width = glfw.get_window_size(window)[0]
 projection = pyrr.matrix44.create_perspective_projection_matrix(fovy=45,aspect=window_width/window_height,near=0.1,far=100)
 #projection = pyrr.matrix44.create_orthogonal_projection_matrix(0,1280,0,720,-1000,1000)
 
-scale = pyrr.matrix44.create_from_scale(pyrr.Vector3([1,1,1]))
+scale = pyrr.matrix44.create_from_scale(pyrr.Vector3([1]*3))
 
-
+ 
 
 # eye pos , target, up
 view = pyrr.matrix44.create_look_at(pyrr.Vector3([0,0,3]),pyrr.Vector3([0,0,0]),pyrr.Vector3([0,1,0]))
 proj_matrix = glGetUniformLocation(shader,"projection")
 
-translation = pyrr.matrix44.create_from_translation(pyrr.Vector3( [0,0,0] ))
+
+initial_offset = middle_point 
+translation = pyrr.matrix44.create_from_translation(pyrr.Vector3(-initial_offset))
 
 
-
+print(f"Initial offset: {initial_offset}")
 
 
 ## Input
-rotation_list = [0,0,0]
-view_list = [0,0,3]
+
+move = 0
+rotation = pyrr.matrix44.create_from_axis_rotation(np.array([0,1,0]),0)
 def keyboard_input_manager(window,key,scancode,action,mods):
-    global view_list
-    translation_val = 0.3
-    if key == glfw.KEY_LEFT and action == glfw.PRESS:
-        view_list[0]-= translation_val
-    elif key == glfw.KEY_RIGHT and action == glfw.PRESS:
-       view_list[0]+= translation_val
-    elif key == glfw.KEY_UP and action == glfw.PRESS:
-        view_list[1]+= translation_val
-    elif key == glfw.KEY_DOWN and action == glfw.PRESS:
-        view_list[1]-= translation_val
-
-
-right_key_pressed = False
-start_cursor_position = (0,0)
-
-def mouse_input_manager(window,button,action,mods):
-    global right_key_pressed
-    global rotation_list
-    global start_cursor_position
-    global cursor_displacement
-    if button==glfw.MOUSE_BUTTON_LEFT:
-        if action == glfw.PRESS:
-            start_cursor_position = glfw.get_cursor_pos(window)
-            right_key_pressed=True
-        if action == glfw.RELEASE:
-            right_key_pressed= False
-   
-
+    global move
+    global rotation
+    if action == glfw.PRESS:
+        pass
+    
+    
     
 
-
-    
-
-
-glfw.set_key_callback(window,keyboard_input_manager)
+glfw.set_key_callback(window,input_handler.keyboard_handler)
 glfw.set_scroll_callback(window,input_handler.scroll_handler)
-glfw.set_mouse_button_callback(window,mouse_input_manager)
+glfw.set_mouse_button_callback(window,input_handler.mouse_handler)
 
 
 
 
-
-def rotate(x,y,z):
-
-    """x,y,z should be degrees"""
-    x*=pi/180
-    y*=pi/180
-    z*=pi/180
-
-    rot_x = pyrr.Matrix44.from_x_rotation(x)
-    
-    rot_y = pyrr.Matrix44.from_y_rotation(y)
-
-    rot_z = pyrr.Matrix44.from_z_rotation(z)
-
-
-rotations_per_screen_vert = 0.1
-rotations_per_screen_hor = 0.5
-previous_displacement = np.array([0,0])
+previous_displacement = np.zeros(2)
 while not glfw.window_should_close(window):
     glfw.poll_events()
 
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
 
-    if right_key_pressed:
+    if input_handler.right_key_pressed:
         current_cursor_position = glfw.get_cursor_pos(window)
-        cursor_displacement = np.array(current_cursor_position ) - np.array(start_cursor_position) - previous_displacement
-        rotation_list[0] += rotations_per_screen_hor *cursor_displacement[0]/window_width
-        rotation_list[1] += rotations_per_screen_vert *cursor_displacement[1]/window_height
+        cursor_displacement = np.array(current_cursor_position ) - np.array(input_handler.start_cursor_position) - previous_displacement
+        input_handler.rotation_list[0] += input_handler.rotations_per_screen_hor * cursor_displacement[0]/window_width
+        input_handler.rotation_list[1] += input_handler.rotations_per_screen_vert * cursor_displacement[1]/window_height
         previous_displacement = cursor_displacement
-        print(cursor_displacement)
+        
     
 
-    rot_x = pyrr.Matrix44.from_x_rotation(rotation_list[1])
+    rot_x = pyrr.Matrix44.from_x_rotation(input_handler.rotation_list[1])
 
-    rot_y = pyrr.Matrix44.from_y_rotation(rotation_list[0])
+    rot_y = pyrr.Matrix44.from_y_rotation(input_handler.rotation_list[0])
 
     rotation = pyrr.matrix44.multiply(rot_x, rot_y)
 
-
-    current_scale = 1
-    scale = pyrr.matrix44.create_from_scale(pyrr.Vector3([current_scale,current_scale,current_scale]))
+  
+   
     view = pyrr.matrix44.create_look_at(pyrr.Vector3(input_handler.eye),pyrr.Vector3(input_handler.target),pyrr.Vector3(input_handler.up))
-    # view = pyrr.matrix44.create_from_translation(pyrr.Vector3(view_list))
-    translation_rotation = pyrr.matrix44.multiply(rotation,translation)
-    model= pyrr.matrix44.multiply(scale,translation_rotation)
+ 
+  
+    model= pyrr.matrix44.multiply(scale,translation)
+    model= pyrr.matrix44.multiply(model,rotation)
     glUniformMatrix4fv(view_loc,1,GL_FALSE,view)
     glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
     glUniformMatrix4fv(proj_matrix, 1, GL_FALSE, projection) 
-    glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+    
+
+    if input_handler.mode:
+        glDrawElements(GL_LINES, len(indices), GL_UNSIGNED_INT, None)
+    else:
+        glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_INT, None)
+
+
+    
 
     glfw.swap_buffers(window)
 
--- src/read_view/read_file.py
+++ src/read_view/read_file.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def ply_to_off(path):
     off_file = ["OFF\n"]
-    with open(path) as f:
+    
+    with path.open() as f:
         ply = f.readlines()
     
     ply = [x for x in ply if not x.startswith("comment")]
@@ -27,18 +28,21 @@ def ply_to_off(path):
 
 
 def read_file(path):
-    if path.split(".")[-1]=="off":
+    line_list = False
+    if path.suffix==".off":
         pass
-    elif path.split(".")[-1]=="ply":
+    elif path.suffix==".ply":
         line_list=ply_to_off(path)
+    else:
+        raise Exception(f"Invalid file type: {path.suffix}")
 
     if not line_list:    
-        with open(path) as f:
+        with path.open() as f:
             line_list=f.readlines()
         line_list = [x for x in line_list if x[0]!= "#"]
     if "OFF" in  line_list[0]:
         line_list=line_list[1:]
-        print("off file")
+        
     
     line_list= [ x.rstrip() for x in line_list]
 
@@ -49,23 +53,25 @@ def read_file(path):
         n_vertices = info[0]
         n_faces= info[1]
         n_edges= info[2]
-        n_cells = info[3]
+        n_attributes = info[3]
     else:
         n_vertices = info[0]
         n_faces= info[1]
-        n_cells = info[2]
+        n_attributes = info[2]
+
         
 
-   
-      
-        vertices = line_list[:n_vertices]
-        vertices = np.array( [list(map(lambda y: float(y),x.split())) for x in vertices],dtype= np.float32).flatten()
-        triangle_elements = line_list[n_vertices:]
-        triangle_elements = np.array([list(map(lambda y: int(y),x.split()))[1:] for x in triangle_elements],dtype=np.uint32).flatten()
+    if n_attributes >0:
+        raise Exception("Extra properties")
+    
+    vertices = line_list[:n_vertices]
+    vertices = np.array( [list(map(lambda y: float(y),x.split())) for x in vertices],dtype= np.float32).flatten()
+    triangle_elements = line_list[n_vertices:]
+    triangle_elements = np.array([list(map(lambda y: int(y),x.split()))[1:] for x in triangle_elements],dtype=np.uint32).flatten()
 
-   
 
-        return vertices, triangle_elements, info
+
+    return vertices, triangle_elements, info
     
 
          
