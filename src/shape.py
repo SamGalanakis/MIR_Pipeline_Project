normalization fix

src/shape.py
@@ -27,7 +27,8 @@ class Shape:
     def process_shape(self):
         self.barycenter =   self.vertices.reshape(-1, 3).mean(axis=0)
         processed_vertices = self.vertices.reshape(-1, 3) - self.barycenter
-        self.processed_vertices = (processed_vertices/np.abs(processed_vertices).max(axis=0)).flatten()
+    
+        self.processed_vertices = (processed_vertices- processed_vertices.min(axis=0))/(processed_vertices.max(axis=0)- processed_vertices.min(axis=0)).flatten()
         self.bounding_rect_vertices, self.bounding_rect_indices = bounding_box(self.processed_vertices,self.element_dict["triangles"])
 
     def view_processed(self):