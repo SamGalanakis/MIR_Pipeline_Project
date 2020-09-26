barycenter rotation fixed

src/model_viewer.py
@@ -61,17 +61,6 @@ class ModelViewer:
 
 
         
-        # surface_normals = []
-        # vertices_accesible = vertices.reshape((-1,3))
-        # for index in range(0,len(indices)-3,3):
-            
-        #     v1 = vertices_accesible[indices[index+1],:] - vertices_accesible[indices[index],:]
-        #     v2 = vertices_accesible[indices[index+2],:] - vertices_accesible[indices[index+1],:]
-        #     v3 = vertices_accesible[indices[index],:] - vertices_accesible[indices[index+2],:]
-        #     surface_normal = pyrr.vector3.generate_normals(v1,v2,v3,normalize_result=True)
-        #     surface_normals.append(surface_normal)
-
-        # surface_normals = np.array(surface_normals)
 
 
        
@@ -105,7 +94,7 @@ class ModelViewer:
 
         as_points = vertices.reshape(-1, 3)
 
-        mean_point = as_points.mean(axis=0)
+        barycenter = as_points.mean(axis=0)
 
         max_x, max_y, max_z = as_points.max(axis=0)
         min_x, min_y, min_z = as_points.min(axis=0)

src/shape.py
@@ -23,7 +23,7 @@ class Shape:
         self.pyvista_mesh = False
         
 
-        self.barycenter =   vertices.reshape(-1, 3).mean(axis=0) #is this barycenter or just centroid??
+        self.barycenter =   vertices.reshape(-1, 3).mean(axis=0) 
 
         self.normalized_barycentered_vertices = vertices.reshape(-1, 3).mean(axis=0) - self.barycenter
 
@@ -38,18 +38,20 @@ class Shape:
                 self.edges.add((a,b))
 
     def process_shape(self,flip=True):
-        self.barycenter =   self.vertices.reshape(-1, 3).mean(axis=0)
-        processed_vertices = self.vertices.reshape(-1, 3)  - self.barycenter
+       
         
        
         max_range = 1
         min_range = 0
-
+        processed_vertices = self.vertices.reshape(-1, 3) 
         scaled_unit = (max_range - min_range) / (np.max(processed_vertices) - np.min(processed_vertices))
 
         self.processed_vertices = processed_vertices*scaled_unit - np.min(processed_vertices)*scaled_unit + min_range
         self.processed_vertices, self.eigenvectors = align(self.processed_vertices.flatten())
-        #self.processed_vertices = flip_test(self.processed_vertices,self.element_dict["triangles"]).astype(np.float32)
+        self.processed_vertices = flip_test(self.processed_vertices,self.element_dict["triangles"]).astype(np.float32)
+
+        self.barycenter =   self.processed_vertices.reshape(-1, 3).mean(axis=0)
+        self.processed_vertices = self.processed_vertices.reshape(-1, 3)  - self.barycenter
         self.bounding_rect_vertices, self.bounding_rect_indices = bounding_box(self.processed_vertices,self.element_dict["triangles"])
     
 
@@ -126,6 +128,7 @@ if __name__ == "__main__":
     vertices, element_dict, info = reader.read(path)
     shape = Shape(vertices,element_dict,info)
     #shape.decimate(0.9)
+    shape.view_processed()
     shape.make_pyvista_mesh_processed()
     shape.pyvista_mesh_processed.plot()
     

src/utils.py
@@ -164,7 +164,7 @@ def two_vertices(vertices):
     
     indices = np.random.choice(vertices.shape[0], int(number_of_vertices), replace=False)
 
-    angles_three_vertices = [calculate_angle(a, b, c) for a, b, c in grouped(vertices[indices], 3)]
+    
     vertices_difference = [np.linalg.norm(a - b) for a, b in grouped(vertices, 2)]
     bins = np.linspace(0, 1, 10)
     binned = np.digitize(vertices_difference, bins)