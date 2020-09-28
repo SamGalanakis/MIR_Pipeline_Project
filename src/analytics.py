4cafba18188d85527dfe69ef0963443d1263603f
lougriz <miguelvalente@protonmail.com>
2020-09-28 18:00:53 +0200

    fix paths.. analytics.py

--- src/analytics.py
+++ src/analytics.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 from shape import Shape
 from model_viewer import ModelViewer
-data_path = Path("processed_data//testingDatamaker.csv")
+data_path = Path(r"processed_data/testingDatamaker.csv")
 
 
 df = pd.read_csv(data_path)
@@ -14,10 +14,11 @@ plt.show()
 
 df_sorted = df.sort_values("n_triangles")
 
-min_example = df_sorted.iloc[0,:]["file_name"]
-max_example = df_sorted.iloc[-1,:]["file_name"]
-mean_example =  df.iloc[(df['n_triangles']-df["n_triangles"].mean()).abs().argsort()[0]]["file_name"]
+min_example = df_sorted.iloc[0,:]["file_name"].replace("\\","/")
+max_example = df_sorted.iloc[-1,:]["file_name"].replace("\\","/")
+mean_example =  df.iloc[(df['n_triangles']-df["n_triangles"].mean()).abs().argsort()[0]]["file_name"].replace("\\","/")
 viewer = ModelViewer()
 
-viewer.process(Path(min_example))
+for  a in df_sorted["file_name"]:
+    viewer.process(Path(a.replace("\\","/")))
 print('done')
