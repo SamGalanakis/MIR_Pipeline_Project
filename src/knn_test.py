ed89cd0d76acf8e1dec4dff0ea79cf3451cabb7e
Samuel Galanakis <sammeg.alanakis6@gmail.com>
2020-10-03 15:41:39 +0200

    query interface outline

--- src/knn_test.py
+++ src/knn_test.py
@@ -22,7 +22,7 @@ class FaissKNeighbors:
         self.index = faiss.IndexFlatL2(X.shape[1])
         self.index.add(X.astype(np.float32))
         self.y = y
-
+    
     def predict(self, X):
         distances, indices = self.index.search(X.astype(np.float32), k=self.k)
         votes = self.y[indices]
