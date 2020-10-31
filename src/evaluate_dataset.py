89736ba4591ac664c9c01539eb5bf33332865b1a
lougriz <miguelvalente@protonmail.com>
2020-10-31 20:11:48 +0100

    graphs

--- src/evaluate_dataset.py
+++ src/evaluate_dataset.py
@@ -27,27 +27,103 @@ class Evaluator:
         self.database_class_path = self.df[["classification","file_name"]]
         self.labels = {}
         self.results = []
-        
         self.class_counts = self.df['classification'].value_counts()
+        self.big_metrics = []
 
     def evaluate(self, baseline = False):
         self.baseline = baseline
+        self.big_results = defaultdict(dict)
         for idx, (classification,model)  in enumerate(self.database_class_path.values):
             #Number of results is based on the number of shapes in a class 
+            to_query = np.arange(5,205,5)
+            results = []
+            for n_results in to_query:
+                if self.baseline:
+                    _, indices = self.faiss_knn.query_baseline(self.df_numeric.iloc[idx].values, n_results)
+                    results.append((classification, indices))
+
+                else:
+                    _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, n_results)
+                    results.append((classification, indices.flatten()))
+
+
+                self.big_results[n_results] = results
+                results = []
+
             n_results = self.class_counts[classification]
 
             if self.baseline:
                 _, indices = self.faiss_knn.query_baseline(self.df_numeric.iloc[idx].values, n_results)
-                self.results.append((classification, indices))
+                results.append((classification, indices))
 
             else:
                 _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, n_results)
-                self.results.append((classification, indices.flatten()))
+                results.append((classification, indices.flatten()))
 
 
-        self.analysis()
+            self.big_results[0] = results
+
+
+
+    def analysis_big(self):   
+        self.overall_accuracy_per_class = 0
+        metrics = {"accuracy" : 0,"precision": 0, "recall" :0 ,"f1" :0}
+        accuracy = []
+        precision = []
+        recall = []
+        f1 = []
+        temp = defaultdict(dict)
+        for n_results, results in self.big_results.items():
+            metrics = {"accuracy" : 0,"precision": 0, "recall" :0 ,"f1" :0}
+            accuracy = []
+            precision = []
+            recall = []
+            f1 = []
+
+            for index, (classification, indices) in enumerate(results):
+                true_positives = sum([self.df["classification"][ind] == classification for ind in indices])
+                false_positives = len(indices) - true_positives
+                false_negatives = self.class_counts[classification] - true_positives
+                true_negatives =  len(self.df) - false_negatives - false_positives - true_positives
+
+                accuracy.append((true_positives + true_negatives) / len(self.df)) 
+                precision.append(true_positives / len(indices))
+                recall.append(true_positives / self.class_counts[classification])
+                if recall[-1] == 0 or precision[-1] == 0:
+                    f1.append(0)
+                else:
+                    f1.append(2 * ((recall[-1] * precision[-1]) / (recall[-1] + precision[-1])))
+
+            metrics["accuracy"] = sum(accuracy) / len(results)
+            metrics["precision"] = sum(precision) / len(results)
+            metrics["recall"] = sum(recall) / len(results)
+            metrics["f1"] = sum(f1) / len(results)
+            del metrics["accuracy"]
+            temp[n_results] = metrics
+
+        return temp
+
+    def heatmap(self,classifications):
+        values = []
+
+        for class_ in set(classifications):
+
+            #ALl queries of a partcular class
+            for model_idx in np.where(self.database_class_path["classification"].values == class_)[0]:
+                distances, indices = self.faiss_knn.query(self.df_numeric.iloc[model_idx].values, len(self.df))
+                classes_indices = list(map(classifications.__getitem__, indices[0]))
+                
+                for clas__ in set(classifications):
+                    a = sum(list(map(distances.__getitem__,[i for i, x in enumerate(classes_indices) if x == clas__])))
+                    print()
+
+              #  a, b = [(sum(list(map(distances.__getitem__,[i for i, x in enumerate(classes_indices) if x == class__]))),class__) for class__ in set(classifications)]
+
+                print()
+
 
     
+
 
     def analysis(self):   
         self.overall_accuracy_per_class = 0
@@ -58,6 +134,7 @@ class Evaluator:
         recall = []
         f1 = []
 
+        
         for index, (classification, indices) in enumerate(self.results):
             true_positives = sum([self.df["classification"][ind] == classification for ind in indices])
             false_positives = len(indices) - true_positives
@@ -97,12 +174,12 @@ class Evaluator:
         self.metrics["precision"] = sum(precision) / len(self.results)
         self.metrics["recall"] = sum(recall) / len(self.results)
         self.metrics["f1"] = sum(f1) / len(self.results)
-
+        del self.metrics["accuracy"]
 
 def make_graphs(test,file_name):
     
     sns.set_style('darkgrid')
-    del test.metrics["accuracy"]
+   # del test.metrics["accuracy"]
     df = pd.DataFrame.from_dict(test.metrics,orient="index")
     df.columns = ["metric"]
    # df.drop(['accuracy'])
@@ -168,20 +245,55 @@ def make_graphs(test,file_name):
 
     print(f"{file_name} is done")
 
+def make_accu_graphs(metrics):
+    sns.set_style('darkgrid')
+   # del test.metrics["accuracy"]
+    df = pd.DataFrame.from_dict(metrics,orient="index")
+    #sns.scatterplot(data=df, x=df.index, y="precision",)
+
+    ax = sns.scatterplot(x=df.index, y="precision", data=df)
+    ax = sns.scatterplot(x=df.index, y="recall", data=df)
+    ax = sns.scatterplot(x=df.index, y="f1", data=df)
+    ax.set(xlabel='Results per query', ylabel='Metrics')
+
+    plt.xticks(
+        rotation=45, 
+        horizontalalignment='right',
+        fontweight='light',
+        fontsize='x-large'  
+    )
+    ax.tick_params(axis='x', labelsize=11)
+    ax.legend(["precision","recall","f1"])
+
+    plt.title(f"Metrics across different results per query")
+
+    plt.tight_layout()
+    plt.savefig(fr"graphs/evaluations/metrics_across_queries",dpi=150)
+    plt.show
+
+    print()
+
+
 
 if __name__ == '__main__':
     #data_path = Path("processed_data/data_processed_10000_1000000.0.csv")
     data_path = Path("processed_data")
 
-    
     for subdir, dirs, files in os.walk(data_path):
         for file_name in files:
             df, *_ = process_dataset_for_knn(os.path.join(subdir, file_name),divide_distributions=False)
             classifications = df['classification'].to_list()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
             test = Evaluator(df)
+            test.heatmap(classifications)
             test.evaluate()
-            make_graphs(test,file_name)
+           # test.analysis()
+           # make_graphs(test,file_name)
+
+            temp = test.analysis_big()
+            make_accu_graphs(temp)
+
+
     
-    baseline_test = Evaluator(df)
-    baseline_test.evaluate(baseline = True)
-    make_graphs(baseline_test,"baseline")
+    #baseline_test = Evaluator(df)
+    #baseline_test.evaluate(baseline = True)
+    #make_graphs(baseline_test,"baseline")
