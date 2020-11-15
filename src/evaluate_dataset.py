a19386f0159ea4b1153e2e29c09ff29f08f6727c
lougriz <miguelvalente@protonmail.com>
2020-11-15 14:27:19 +0100

    clean

--- src/evaluate_dataset.py
+++ src/evaluate_dataset.py
@@ -29,104 +29,43 @@ class Evaluator:
         self.faiss_knn.train()
       
         self.database_class_path = self.df[["classification","file_name"]]
-        self.labels = {}
-        self.results = []
         self.class_counts = self.df['classification'].value_counts()
-        self.big_metrics = []
 
-    def evaluate(self, baseline = False):
-        self.baseline = baseline
-        self.big_results = defaultdict(dict)
-
-        for idx, (classification,model)  in enumerate(self.database_class_path.values):
-            #Number of results is based on the number of shapes in a class 
-            n_results = self.class_counts[classification]
-
-<<<<<<< Updated upstream
-            if self.data_path:
-                _, indices = self.custom_knn.query(self.df_numeric.iloc[idx].values, n_results,self.weights)
-                self.results.append((classification, indices.flatten()))
-
-
-            elif self.baseline:
-                _, indices = self.faiss_knn.query_baseline(n_results)
-=======
-            
-            if self.baseline:
-
-                _, indices = self.faiss_knn.query_baseline(self.df_numeric.iloc[idx].values, n_results)
->>>>>>> Stashed changes
-                self.results.append((classification, indices))
-            else:
-                _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, n_results)
-                self.results.append((classification, indices.flatten()))
-
-    def evaluate_big(self, baseline = False):
+    def evaluate(self, baseline = False,max_query=205):
             self.baseline = baseline
-            self.big_results = defaultdict(dict)
+            self.all_results = defaultdict(dict)
                 #Number of results is based on the number of shapes in a class 
-<<<<<<< Updated upstream
-                to_query = np.arange(5,205,5)
-                results = []
-
-
-                for n_results in to_query:
-                    if self.baseline:
-                        _, indices = self.faiss_knn.query_baseline( n_results)
-                        results.append((classification, indices))
-
-                    else:
-                        _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, n_results)
-                        results.append((classification, indices.flatten()))
-=======
-            to_query = np.arange(5,55,5)
+            to_query = np.arange(5,max_query+5,5)
             results = []
->>>>>>> Stashed changes
 
-            for idx, (classification,model)  in enumerate(self.database_class_path.values):
-                
+            for idx, (classification,model)  in enumerate(self.database_class_path.values):    
                 if baseline:
-                    _, indices = self.faiss_knn.query_baseline(self.df_numeric.iloc[idx].values, 205)
+                    _, indices = self.faiss_knn.query_baseline(max_query)
                     results.append((classification, indices))
                 else:
-                    _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, 205)                
+                    _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, max_query)                
                     results.append((classification, indices.flatten()))
 
             for n_results in to_query:
-                
-
-                self.big_results[n_results] = [(re[0],re[1][0:n_results]) for re in results]
+                self.all_results[n_results] = [(re[0],re[1][0:n_results]) for re in results]
                 
             results =  []
             for idx, (classification,model)  in enumerate(self.database_class_path.values):
-                
                 n_results = self.class_counts[classification]
-<<<<<<< Updated upstream
-
-                if self.baseline:
-                    _, indices = self.faiss_knn.query_baseline( n_results)
-=======
                 if baseline:
-                    _, indices = self.faiss_knn.query_baseline(self.df_numeric.iloc[idx].values, n_results)
->>>>>>> Stashed changes
+                    _, indices = self.faiss_knn.query_baseline(n_results)
                     results.append((classification, indices))
                 else:
                     _, indices = self.faiss_knn.query(self.df_numeric.iloc[idx].values, n_results)                
                     results.append((classification, indices.flatten()))
 
 
-            self.big_results[0] = results
+            self.all_results[0] = results
             print("Done evaluate")
 
-    def analysis_big(self):   
-        self.overall_accuracy_per_class = 0
-        metrics = {"accuracy" : 0,"precision": 0, "recall" :0 ,"f1" :0}
-        accuracy = []
-        precision = []
-        recall = []
-        f1 = []
-        temp = defaultdict(dict)
-        for n_results, results in self.big_results.items():
+    def analysis(self):   
+        metrics_per_querysize = defaultdict(dict)
+        for n_results, results in self.all_results.items():
             metrics = {"accuracy" : 0,"precision": 0, "recall" :0 ,"f1" :0}
             accuracy = []
             precision = []
@@ -152,138 +91,12 @@ class Evaluator:
             metrics["recall"] = sum(recall) / len(results)
             metrics["f1"] = sum(f1) / len(results)
             del metrics["accuracy"]
-            temp[n_results] = metrics
+            metrics_per_querysize[n_results] = metrics
 
         print("Done analysis")
-        return temp
+        return metrics_per_querysize
 
-    def analysis(self):   
-        self.overall_accuracy_per_class = 0
-        self.metrics = {"accuracy" : 0,"precision": 0, "recall" :0 ,"f1" :0}
-        self.metrics_per_class =  {"accuracy" : defaultdict(list),"precision": defaultdict(list), "recall" :defaultdict(list) ,"f1" :defaultdict(list)}
-        accuracy = []
-        precision = []
-        recall = []
-        f1 = []
-
-        
-        for classification, indices in self.results:
-            true_positives = sum([self.df["classification"][ind] == classification for ind in indices])
-            false_positives = len(indices) - true_positives
-            false_negatives = self.class_counts[classification] - true_positives
-            true_negatives =  len(self.df) - false_negatives - false_positives - true_positives
-
-            accuracy.append((true_positives + true_negatives) / len(self.df)) 
-            precision.append(true_positives / len(indices))
-            recall.append(true_positives / self.class_counts[classification])
-            if self.baseline:
-                if recall[-1] == 0 or precision[-1] == 0:
-                    f1.append(0)
-                else:
-                    f1.append(2 * ((recall[-1] * precision[-1]) / (recall[-1] + precision[-1])))
-
-            else:
-                f1.append(2 * ((recall[-1] * precision[-1]) / (recall[-1] + precision[-1])))
-
-            self.metrics_per_class["accuracy"][classification].append(accuracy[-1])
-            self.metrics_per_class["precision"][classification].append(precision[-1])
-            self.metrics_per_class["recall"][classification].append(recall[-1])
-            self.metrics_per_class["f1"][classification].append(f1[-1])
-        
-        self.metrics_per_class_weigthed = self.metrics_per_class.copy()
-        self.metrics_weigthed = self.metrics.copy()
-
-        for metric_key, classes  in self.metrics_per_class.items():
-            for classification, metric in classes.items():
-                self.metrics_per_class[metric_key][classification] = sum(metric) / len(metric)
-                self.metrics_per_class_weigthed[metric_key][classification] = ((sum(metric) / len(metric)) * self.class_counts[classification]) / sum(self.class_counts)
-            
-        for metric_key, classes in self.metrics_per_class_weigthed.items():
-            self.metrics_weigthed[metric_key] = sum(classes.values()) 
-
-
-        self.metrics["accuracy"] = sum(accuracy) / len(self.results)
-        self.metrics["precision"] = sum(precision) / len(self.results)
-        self.metrics["recall"] = sum(recall) / len(self.results)
-        self.metrics["f1"] = sum(f1) / len(self.results)
-        del self.metrics["accuracy"]
-        return self.metrics
-
-
-def make_graphs(test,file_name,devided):
-    
-    sns.set_style('darkgrid')
-   # del test.metrics["accuracy"]
-    df = pd.DataFrame.from_dict(test.metrics,orient="index")
-    df.columns = ["metric"]
-   # df.drop(['accuracy'])
-    ax = sns.barplot(x=df.index, y="metric", data=df)
-    plt.xticks(
-        rotation=45, 
-        horizontalalignment='right',
-        fontweight='light',
-        fontsize='x-large'  
-    )
-    ax.tick_params(axis='x', labelsize=11)
-
-    file_name = file_name.replace(".","")
-    
-    if file_name == "baseline":
-        plt.title(f"Average metrics baseline")
-    elif "coarse" and "processed" in file_name:
-        plt.title(f"Average metrics for preprocessed coarse shapes")
-    elif "processed" in file_name:
-        plt.title(f"Average metrics for preprocessed shapes")
-    else:
-        plt.title(f"Average metrics for unprocessed shapes")
-
-    plt.tight_layout()
-    if devided:
-        plt.savefig(fr"graphs/evaluations/devided_{file_name}_all_metrics",dpi=150)
-    else:
-        plt.savefig(fr"graphs/evaluations/{file_name}_all_metrics",dpi=150)
-    plt.clf()
-
-    for metric, classes in test.metrics_weigthed.items():
-        
-        if metric == "accuracy":
-            continue
-        df = pd.DataFrame.from_dict(classes,orient="index")
-        df.columns = [metric]
-        ax = sns.barplot(x=df.index, y=metric, data=df)
-
-        if file_name == "baseline":
-            plt.title(f"{metric} per class metrics baseline")
-        elif "processed" in file_name:
-            plt.title(f"{metric} per class for preprocessed shapes")
-        else:
-            plt.title(f"{metric} per class for unprocessed shapes")
-
-        if "coarse" in file_name:  
-            ax.set_xticklabels(ax.get_xticklabels(), rotation=90, horizontalalignment='right', fontweight='light',fontsize='x-large' )
-            ax.tick_params(axis='x', labelsize=7)
-        else:
-            ax = sns.barplot(x=df.index, y=metric, data=df)
-            ax.set_xticklabels([], horizontalalignment='right', fontweight='light',fontsize='x-large' )
-           # plt.xticks(
-           #     rotation=45, 
-           #     horizontalalignment='right',
-           #     fontweight='light',
-           #     fontsize='x-small'  
-           # )
-        ax.tick_params(axis='x', labelsize=7)
-
-
-        plt.tight_layout()
-        if devided:
-            plt.savefig(fr"graphs/evaluations/devided_{file_name}_{metric}",dpi=150)
-        else:
-            plt.savefig(fr"graphs/evaluations/{file_name}_{metric}",dpi=150)
-        plt.clf()
-
-    print(f"{file_name} is done")
-
-def make_accu_graphs(metrics,file_name,devided):
+def make_graphs(metrics,file_name,devided):
     file_name = file_name.replace(".","")
 
     sns.set_style('darkgrid')
@@ -329,8 +142,6 @@ def make_accu_graphs(metrics,file_name,devided):
     plt.clf()
 
 
-
-
 if __name__ == '__main__':
     #data_path = Path("processed_data/data_processed_10000_1000000.0.csv")
                 
@@ -339,26 +150,11 @@ if __name__ == '__main__':
     class_dicts = [get_princeton_classifications(cla_path) for cla_path in cla_paths]
     df, *_ = process_dataset_for_knn("processed_data/data_coarse1_processed_10000_10000000.0.csv",divide_distributions)
     
-
-    #for idx, class_dict in enumerate(class_dicts):
-    #    df['classification'] = df.file_name.map(lambda x: class_dict[os.path.basename(x).split(".")[0].replace("m","")])
-#
-    #    #classifications = df['classification'].to_list()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-    #    #test = Evaluator(df)
-    #    #test.evaluate()
-    #    #test.analysis()
-    #    #make_graphs(test,f"data_coarse{idx+1}_processed",divide_distributions)
-#
-    #    test = Evaluator(df)
-    #    test.evaluate_big()
-    #    temp = test.analysis_big()
-    #    make_accu_graphs(temp,f"data_coarse{idx+1}_processed",divide_distributions)
-#
     baseline_test = Evaluator(df)
-    baseline_test.evaluate_big(baseline = True)
-    temp=baseline_test.analysis_big()
+    baseline_test.evaluate(baseline = True)
+    temp = baseline_test.analysis()
 
-    make_accu_graphs(temp,"baseline",True)
-#
+
+
    # profiler.run('database.create_database(base_name,n_samples=n_samples,apply_processing=apply_processing,n_vertices_target=n_vertices_target)')
    # profiler.dump_stats("query")
